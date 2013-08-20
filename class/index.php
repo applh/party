e1c59708f5cc633ddde7d69c9761610f0f18cb8f
Applh.com <github@applh.com>
2013-08-20 17:48:45 +0200

    add handling of user config
    * manage /party-config.php

--- class/index.php
+++ class/index.php
@@ -1,18 +1,29 @@
 <?php
 
+global $Party;
+if (!is_array($Party)) {
+   $Party=array();
+   $Party['src.url.domain']='http://applh.com';
+}
+
 if (!function_exists('party_curl')) {
    function party_curl () {
+      global $Party;
 
+      // FIXME
       // request parsing
-      $request2url=$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+      $request2url='http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
       $request2parse=parse_url($request2url);
       $request2path=$request2parse['path'];
       
       $request2pathinfo=pathinfo($request2path);
-      $request2ext=$request2pathinfo['extension'];
+      $request2ext=strtolower(trim($request2pathinfo['extension']));
       
       // source data
-      $src2url2domain="http://applh.com";
+      $src2url2domain=$Party['src.url.domain'];
+
+      // FIXME
+      // curl is ok with GET query string inside CURLOPT_URL
       $src2uri=$_SERVER['REQUEST_URI'];
 
       $src_url="$src2url2domain$src2uri";
@@ -37,9 +48,49 @@ if (!function_exists('party_curl')) {
       // Fermeture de la session cURL
       curl_close($ch);
 
-      if (stripos($headers['Accept'], "text/html") !== FALSE) {
-         header("Content-Type:text/html");
-         echo $result;
+      
+      if (stripos($headers['Accept'], "text/") !== FALSE) {
+         if (!empty($request2ext)) {
+            switch ($request2ext) {
+               case 'txt':
+                  header("Content-Type:text/plain");
+                  echo $result;
+                  break;
+               case 'css':
+                  header("Content-Type:text/css");
+                  echo $result;
+                  break;
+               case 'js':
+                  header("Content-Type:text/javascript");
+                  echo $result;
+                  break;
+               case 'htm':
+               case 'html':
+                  header("Content-Type:text/html");
+                  echo $result;
+                  break;
+               default:
+                  header("Content-Type:text/html");
+                  echo $result;
+                  // DEBUG
+                  //header("Content-Type:text/plain");
+                  //print_r($request2parse);
+                  //print_r($request2pathinfo);
+                  break;
+            }
+         }
+         else if (stripos($headers['Accept'], "text/html") !== FALSE) {
+            header("Content-Type:text/html");
+            echo $result;
+         }
+         else if (stripos($headers['Accept'], "text/css") !== FALSE) {
+            header("Content-Type:text/css");
+            echo $result;
+         }
+         else if (stripos($headers['Accept'], "text/javascript") !== FALSE) {
+            header("Content-Type:text/javascript");
+            echo $result;
+         }
       }
       else if (stripos($headers['Accept'], "image/") !== FALSE) {
 
@@ -68,8 +119,13 @@ if (!function_exists('party_curl')) {
 
 if (!function_exists('party')) {
    function party () {
+      // FIXME
+      $party2config=dirname(dirname(__DIR__))."/party-config.php";
+      if (file_exists($party2config)) {
+         include($party2config);
+      }
 
-     party_curl();
+      party_curl();
    }
 }
 
