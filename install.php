9cc824fac7015ded07e77e66e9c9c23ede0cf736
Applh.com <github@applh.com>
2013-08-21 21:00:19 +0200

    add /party/install.php
    basic file...

---
+++ install.php
@@ -0,0 +1,14 @@
+<?php
+
+$curdir=__DIR__;
+$docroot=$_SERVER['DOCUMENT_ROOT'];
+
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+   <title>PARTY * INSTALL</title>
+   </head>
+   <body>
+   </body>
+</html>
